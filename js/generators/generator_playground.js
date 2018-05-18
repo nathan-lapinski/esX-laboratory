7e82e4114296a15d8ebf67a7d484f64e9f1e36cf
Nathan Lapinski <n4t311@gmail.com>
2018-05-18 20:21:34 +0900

    adding generator examples

--- js/generators/generator_playground.js
+++ js/generators/generator_playground.js
@@ -14,3 +14,51 @@ const megamanBosses = {
 for (const boss of megamanBosses) {
     console.log('iterated: ', boss);
 }
+
+// Fibonacci time
+
+function fibonacciGen() {
+  let [a,b] = [0,1];
+  const iterator = {
+    next: () => {
+        const ret = { value: a, done: false };
+        [a,b] = [b, a + b];
+        return ret;
+    }
+  }
+  return iterator;
+}
+
+let iterator = fibonacciGen();
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+function * trueFibGen() {
+    let [a,b] = [0,1];
+    while(true) {
+        let ret = a;
+        [a, b] = [b, a + b];
+        yield ret;
+    }
+}
+console.log('now with syntactic sugar');
+const it = trueFibGen();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
